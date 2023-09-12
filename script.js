a32932c83a55d45f4b81165cf71c550b3b6fb77d
Vinay Vastrakar <101122381+VinayVastrakar@users.noreply.github.com>
2023-09-12 17:54:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let char;
+	for(let i=0; i<str.length; i++){
+		if(str.indexOf(srt[i])=== str.lasatIndexOf(srt[i])){
+			char = str[i];
+			break;
+		}
+	}
+	return char;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
